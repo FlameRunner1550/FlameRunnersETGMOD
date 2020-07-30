1994ac17d22f2513b7ce6324c96aae3b85da0d67
FlameRunner1550 <Patrick@Sheafer.org>
2020-07-30 14:34:44 -0400

    Add files via upload

---
+++ Amulet of Yondor.cs
@@ -0,0 +1,60 @@
+﻿using ItemAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace ExampleMod
+{
+   public class AmuletOfYondor : PassiveItem
+    {
+        public static void Register()
+        {
+            //The name of the item
+            string itemName = "Amulet of Yondor";
+
+            //Refers to an embedded png in the project. Make sure to embed your resources! Google it
+            string resourceName = "ExampleMod/Resources/pixil-frame-0";
+
+            //Create new GameObject
+            GameObject obj = new GameObject(itemName);
+
+            //Add a PassiveItem component to the object
+            var item = obj.AddComponent<AmuletOfYondor>();
+
+            //Adds a sprite component to the object and adds your texture to the item sprite collection
+            ItemBuilder.AddSpriteToObject(itemName, resourceName, obj);
+
+            //Ammonomicon entry variables
+            string shortDesc = "One Hell Of An Amulet";
+            string longDesc = "Increases health, speed, and rate of fire\n\n" +
+                "Legends have it that in a far off dungeon, adventures would seek out an amulet that would let them become more powerful than god himself. \n" + 
+                "It appears that this is a poorly made tribute for the legendary amulet's cousin, Yondor.";
+
+            //Adds the item to the gungeon item list, the ammonomicon, the loot table, etc.
+            //Do this after ItemBuilder.AddSpriteToObject!
+            ItemBuilder.SetupItem(item, shortDesc, longDesc, "frm");
+
+            //Adds the actual passive effect to the item
+            ItemBuilder.AddPassiveStatModifier(item, PlayerStats.StatType.Health, 1, StatModifier.ModifyMethod.ADDITIVE);
+            ItemBuilder.AddPassiveStatModifier(item, PlayerStats.StatType.MovementSpeed, 2, StatModifier.ModifyMethod.ADDITIVE);
+            ItemBuilder.AddPassiveStatModifier(item, PlayerStats.StatType.RateOfFire, 1, StatModifier.ModifyMethod.ADDITIVE);
+            //Set the rarity of the item
+            item.quality = PickupObject.ItemQuality.A;
+        }
+
+        public override void Pickup(PlayerController player)
+        {
+            base.Pickup(player);
+            Tools.Print($"Player picked up {this.DisplayName}");
+        }
+
+        public override DebrisObject Drop(PlayerController player)
+        {
+            Tools.Print($"Player dropped {this.DisplayName}");
+            return base.Drop(player);
+        }
+
+    }
+}
---
+++ Worn out Patch.cs
@@ -0,0 +1,63 @@
+﻿using ItemAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace ExampleMod
+{
+
+    
+        public class Wornoutpatch : PassiveItem
+        {
+            //Call this method from the Start() method of your ETGModule extension
+            public static void Register()
+            {
+                //The name of the item
+                string itemName = "Worn Out Patch";
+
+                //Refers to an embedded png in the project. Make sure to embed your resources! Google it
+                string resourceName = "ExampleMod/Resources/Wornoutpatch";
+
+                //Create new GameObject
+                GameObject obj = new GameObject(itemName);
+
+                //Add a PassiveItem component to the object
+                var item = obj.AddComponent<Wornoutpatch>();
+
+                //Adds a sprite component to the object and adds your texture to the item sprite collection
+                ItemBuilder.AddSpriteToObject(itemName, resourceName, obj);
+
+                //Ammonomicon entry variables
+                string shortDesc = "Do A Good Turn Daily";
+            string longDesc = "Increases Item capacity and ammo capacity\n\n" +
+                "Worn by youth around the world as a symbol of their organization.\n\n" + "Someone, somewhere, is probably pissed that this old patch fell off.";
+
+                //Adds the item to the gungeon item list, the ammonomicon, the loot table, etc.
+                //Do this after ItemBuilder.AddSpriteToObject!
+                ItemBuilder.SetupItem(item, shortDesc, longDesc, "frm");
+
+                //Adds the actual passive effect to the item
+                ItemBuilder.AddPassiveStatModifier(item, PlayerStats.StatType.AdditionalItemCapacity, 1, StatModifier.ModifyMethod.ADDITIVE);
+                ItemBuilder.AddPassiveStatModifier(item, PlayerStats.StatType.AmmoCapacityMultiplier, 2, StatModifier.ModifyMethod.MULTIPLICATIVE);
+
+                //Set the rarity of the item
+                item.quality = PickupObject.ItemQuality.B;
+            }
+
+            public override void Pickup(PlayerController player)
+            {
+                base.Pickup(player);
+                Tools.Print($"Player picked up {this.DisplayName}");
+            }
+
+            public override DebrisObject Drop(PlayerController player)
+            {
+                Tools.Print($"Player dropped {this.DisplayName}");
+                return base.Drop(player);
+            }
+        }
+    }
+
+
